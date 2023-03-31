b39336e1b5f4fd5dc8b261b19c1e3901d4eb6719
kellylau <kellylaup@gmail.com>
2023-03-30 20:56:00 -0400

    updated

--- funcs.h
+++ funcs.h
@@ -1,3 +1,9 @@
 #pragma once
 
-// add prototypes here
+#include "coord3d.h"
+
+double length(Coord3D* p);
+Coord3D * fartherFromOrigin(Coord3D, Coord3D); 
+void move(Coord3D*, Coord3D*, double); 
+Coord3D* createCoord3D(double, double, double);
+void deleteCoord3D(Coord3D);
