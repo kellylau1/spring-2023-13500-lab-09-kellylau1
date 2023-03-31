bf9960e2949ef5c3a702fef2e595a36e52e456a4
kellylau <kellylaup@gmail.com>
2023-03-30 21:08:50 -0400

    updated

--- main.cpp
+++ main.cpp
@@ -26,9 +26,9 @@ int main()
     Coord3D *ppos = createCoord3D(x,y,z);
     Coord3D *pvel = createCoord3D(x,y,z);
 
-    move(ppos, pvel, 10.0);
+    move(&ppos, &pvel, 10.0);
 
-    std::cout << "Coordinates after 10 seconds: " << (*ppos).x << " " << (*ppos).y << " " << (*ppos).z << std::endl;
+    std::cout << "Coordinates after 10 seconds: " << (ppos).x << " " << (ppos).y << " " << (ppos).z << std::endl;
 
     deleteCoord3D(ppos); // release memory
     deleteCoord3D(pvel);
